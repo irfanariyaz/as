3bee4d3e00ba20daea4071bb918d5f335e1a8e80
irfanariyaz <32553276+irfanariyaz@users.noreply.github.com>
2017-12-24 21:56:58 -0500

    models

---
+++ models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class RestaurantLocation(models.Model):
+	Name            =models.CharField(max_length=120)
+	location        =models.CharField(max_length=120,null=True,blank=True)
+	category        =models.CharField(max_length=120,null=True,blank=True)
+	timestamp       =models.DateTimeField(auto_now_add=True)
+	updated    		=models.DateTimeField(auto_now=True)
+	my_time_field   =models.DateField(auto_now=False,auto_now_add=False)

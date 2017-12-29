90de44dcefe8926e0428c5f148eb5f0dd75bc161
irfanariyaz <32553276+irfanariyaz@users.noreply.github.com>
2017-12-28 22:57:56 -0500

    introducing queryset

--- models.py
+++ models.py
@@ -5,9 +5,14 @@ from django.db import models
 
 # Create your models here.
 class RestaurantLocation(models.Model):
-	Name            =models.CharField(max_length=120)
+
+	name            =models.CharField(max_length=120)
 	location        =models.CharField(max_length=120,null=True,blank=True)
 	category        =models.CharField(max_length=120,null=True,blank=True)
-	timestamp       =models.DateTimeField(auto_now_add=True)
-	updated    		=models.DateTimeField(auto_now=True)
-	my_time_field   =models.DateField(auto_now=False,auto_now_add=False)
+	timestamp =models.DateTimeField(auto_now_add=True)
+	updated =models.DateTimeField(auto_now=True)
+ #    timestamp       =models.DateTimeField(auto_now_add=True)
+	# updated    		=models.DateTimeField(auto_now=True)
+	#my_time_field   =models.DateField(auto_now=False,auto_now_add=False)
+	def __str__(self):
+		return self.name
